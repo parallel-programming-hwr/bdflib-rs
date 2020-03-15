2dc3cb40b6e9fa15cc5bd8febbce509dc1b341ee
trivernis <trivernis@gmail.com>
2020-03-15 11:53:29 +0100

    Expose thread_manager for debugging purposes

--- src/io.rs
+++ src/io.rs
@@ -12,7 +12,7 @@ use crossbeam_utils::sync::WaitGroup;
 const ENTRIES_PER_CHUNK: u32 = 100_000;
 
 #[derive(Debug)]
-struct ThreadManager<T1, T2> {
+pub struct ThreadManager<T1, T2> {
     pub sender_work: Sender<T1>,
     pub receiver_work: Receiver<T1>,
     pub sender_result: Sender<T2>,
@@ -38,7 +38,7 @@ pub struct BDFWriter {
     head_written: bool,
     compressed: bool,
     compression_level: u32,
-    thread_manager: ThreadManager<GenericChunk, Vec<u8>>,
+    pub thread_manager: ThreadManager<GenericChunk, Vec<u8>>,
 }
 
 impl<T1, T2> ThreadManager<T1, T2> {
